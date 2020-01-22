447fd183d4f4eb27a8893bf8362ae2f35a8b4f6a
guillaumedalbignac <dalbignacguillaume@gmail;com>
2020-01-22 10:18:50 +0100

    Soustraction noir et blanc

--- OpenCV/mainwindow.cpp
+++ OpenCV/mainwindow.cpp
@@ -4,20 +4,20 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    //Lire l'image1
     cv::Mat image1 = cv::imread("C:/Users/dalbi/Desktop/testPhoto/soustraction1.png", 1);
-    //Lire l'image 2
-    cv::Mat image2 = cv::imread("C:/Users/dalbi/Desktop/testPhoto/soustraction2.png", 1);
-
-    //Afficher l'image 1 et 2
     cv::imshow("Image 1", image1);
+    cv::Mat image2 = cv::imread("C:/Users/dalbi/Desktop/testPhoto/soustraction2.png", 1);
     cv::imshow("Image 2", image2);
+    cv::Mat resultat;
+    cv::subtract(image1, image2, resultat);
+    cv::imshow("Soustraction", resultat);
 
 }
 
