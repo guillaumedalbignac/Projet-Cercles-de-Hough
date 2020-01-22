71c04a05dc8ae0af60ff91c6bc908b2c61b13f24
guillaumedalbignac <dalbignacguillaume@gmail;com>
2020-01-22 09:48:56 +0100

    Mise en place de test de soustraction d'images en noir et blancs

--- OpenCV/mainwindow.cpp
+++ OpenCV/mainwindow.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //Lire l'image1
+    cv::Mat image1 = cv::imread("C:/Users/dalbi/Desktop/testPhoto/soustraction1.png", 1);
+    //Lire l'image 2
+    cv::Mat image2 = cv::imread("C:/Users/dalbi/Desktop/testPhoto/soustraction2.png", 1);
+
+    //Afficher l'image 1 et 2
+    cv::imshow("Image 1", image1);
+    cv::imshow("Image 2", image2);
 
 }
 
